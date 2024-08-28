9df210b88162b6a8e8a6210460d8c75379835491
leesh9 <lokciginer@gmail.com>
2024-08-28 10:53:07 +0900

    all 에 length 추가

--- Doker_Study/work/backend/src/main/java/com/sist/backend/controller/EmpController.java
+++ Doker_Study/work/backend/src/main/java/com/sist/backend/controller/EmpController.java
@@ -1,12 +1,15 @@
 package com.sist.backend.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sist.backend.service.EmpService;
+import com.sist.backend.vo.EmpVO;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -19,8 +22,11 @@ public class EmpController {
     @RequestMapping("/all")
     public Map<String, Object> all() {
         Map<String, Object> map = new HashMap<>();
-        map.put("ar", empService.getAll());
-        
+        List<EmpVO> list = empService.getAll();
+        if(list != null) {
+            map.put("length", list.size());
+        }
+        map.put("ar", list);
         return map;
     }
     
